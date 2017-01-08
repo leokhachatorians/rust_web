0ba96955b9052c2a859cf2d8954bfc1655b5bf1c
Leo Khachatorians <leokhachatorians@gmail.com>
2017-01-08 15:28:06 -0800

    first commit; multithreade?

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io::{Read, Write, BufReader, BufRead};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn main() {
+    let addr = "127.0.0.1:8000";
+    let listener = TcpListener::bind(addr).unwrap();
+
+    for stream in listener.incoming() {
+        handle_connection(stream.unwrap());
+    }
+}
+
+fn handle_connection(stream: TcpStream) {
+    thread::spawn(|| {
+        let stream = stream;
+        println!("{:?}", stream);
+        let mut reader = BufReader::new(stream);
+
+        for line in reader.by_ref().lines() {
+            if line.unwrap() == "" {
+                break;
+            }
+        }
+        let response = "HTTP/1.1 200 OK\n\n<html><body>Hello, world!</body></html>";
+        reader.into_inner().write_all(response.as_bytes()).unwrap();
+    });
+}
