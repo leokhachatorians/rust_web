94ff6bdd6dab49cd49cb63551f7612747e77b40c
Leo Khachatorians <leokhachatorians@gmail.com>
2017-01-12 22:26:03 -0800

    echo server test

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,33 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     println!("{:?}", stream);
-    let mut reader = BufReader::new(stream);
-
-    for line in reader.by_ref().lines() {
-        let line = match line {
-            Ok(v) => v,
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
-            }
-        };
-        if line == "" {
-            break;
-        }
+    loop {
+        let mut buf = [0;100];
+        let line = stream.read(&mut buf).unwrap);
+       // let line = match stream.read(&mut buf) {
+       //     Ok(v) => v,
+       //     Err(e) => {
+       //         println!("Error: {}", e);
+       //         break;
+       //     }
+       // };
+        stream.write(&buf[0..line]);
     }
-    let response = "HTTP/1.1 200 OK\n\n<html><body>Hello, world!</body></html>";
-    reader.into_inner().write_all(response.as_bytes()).unwrap();
-   // for i in &vec {
-   //     reader.into_inner().write(i.as_bytes()).unwrap();
+
+   // for line in reader.by_ref().lines() {
+   //     let line = match line {
+   //         Ok(v) => v,
+   //         Err(e) => {
+   //             println!("Error: {}", e);
+   //             break;
+   //         }
+   //     };
+   //     if line == "" {
+   //         break;
+   //     }
    // }
+   // let response = "HTTP/1.1 200 OK\n\n<html><body>Hello, world!</body></html>";
+   // reader.into_inner().write_all(response.as_bytes()).unwrap();
 }
